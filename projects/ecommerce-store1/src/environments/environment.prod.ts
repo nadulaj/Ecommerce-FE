68f7e717694f9a533e4591fad82a932acafd2d49
vidula dantanarayana <vidula.dantanarayana@spemai.com>
2023-02-01 03:33:03 +0530

    made change

--- projects/ecommerce-store1/src/environments/environment.prod.ts
+++ projects/ecommerce-store1/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //apiBaseUrl: "https://202x2vcym8.execute-api.ap-southeast-1.amazonaws.com/development/",
-  apiBaseUrl: "https://202x2vcym8.execute-api.ap-southeast-1.amazonaws.com/live/",
+  apiBaseUrl:"http://localhost:3000/",
+  apiBaseUrl2:"https://202x2vcym8.execute-api.ap-southeast-1.amazonaws.com/live/",
   aws: 'https://onepayserviceimages.s3.amazonaws.com/',
 };
